086c9b0cf20546c425ee52b28156b12ff55d74f3
Ursulaemily <emilywaithira18@gmail.com>
2024-06-04 19:29:00 +0300

    updates

--- ecommerceProject/settings.py
+++ ecommerceProject/settings.py
@@ -206,6 +206,11 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
+# settings.py
+import os
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
--- ecommerceProject/urls.py
+++ ecommerceProject/urls.py
@@ -29,13 +29,6 @@ urlpatterns = [
     path('user_app/', include('user_app.urls')),
     path('', include('store.urls')),
     path('ml/', include('ml.urls')),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-# Serving static files during development
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
+]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
