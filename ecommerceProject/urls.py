c90eeb9c48c6a4aa0522b8bab69b7fe314293ef7
Ursulaemily <emilywaithira18@gmail.com>
2024-06-06 11:40:40 +0300

    updates

--- ecommerceProject/urls.py
+++ ecommerceProject/urls.py
@@ -9,8 +9,7 @@ urlpatterns = [
     path('', include('store.urls')),
     path('ml/', include('ml.urls')),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 
-# if settings.DEBUG:
-#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
