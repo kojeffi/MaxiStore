0e10a567e56bd5a5c40c555fb4aaf70015d6cfc3
kojeffi <omondijeff88@gmail.com>
2024-06-12 16:48:16 +0300

    updates

--- ecommerceProject/settings.py
+++ ecommerceProject/settings.py
@@ -50,7 +50,9 @@ CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap4'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Machine learning API
-ML_API_URL = 'http://localhost:8000/ml'
+# ML_API_URL = 'http://localhost:8000/ml'
+
+ML_API_URL = ''
 
 # Logging Configuration
 LOGGING = {
