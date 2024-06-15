3a354fe13ab8bff43ffbd9402e0465e7ea879686
kojeffi <omondijeff88@gmail.com>
2024-06-15 14:58:31 +0300

    updates

--- store/admin.py
+++ store/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from .models import Product, Cart, CartItem, Order, OrderItem, Review, Wishlist, BlogPost, FAQ, PolicyPage
 
-admin.site.register(Product)
+from .models import Product
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price', 'stock', 'category', 'created_at']
+
 admin.site.register(Cart)
 admin.site.register(CartItem)
 admin.site.register(Order)
