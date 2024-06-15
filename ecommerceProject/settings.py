31b0dad6e34d9cbdbddaea7483cb99224d56a9dd
kojeffi <omondijeff88@gmail.com>
2024-06-15 16:25:51 +0300

    updates

--- ecommerceProject/settings.py
+++ ecommerceProject/settings.py
@@ -48,25 +48,16 @@ INSTALLED_APPS = [
     'cloudinary_storage',
 ]
 
-# import os
+import os
 
-# CLOUDINARY_STORAGE = {
-#     'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME'),
-#     'API_KEY': os.getenv('CLOUDINARY_API_KEY'),
-#     'API_SECRET': os.getenv('CLOUDINARY_API_SECRET'),
-# }
-
-# DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
-
-
-# Using environment variables for Cloudinary credentials
 CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME', 'dnqsiqqu9'),
-    'API_KEY': os.getenv('CLOUDINARY_API_KEY', '155569521244935'),
-    'API_SECRET': os.getenv('CLOUDINARY_API_SECRET', 'gr59xu00MEcd8EJJvZWP4TZGtoU'),  # replace with actual secret
+    'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.getenv('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.getenv('CLOUDINARY_API_SECRET'),
 }
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap4'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
